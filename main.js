1dd10bf65bef94a9de6e242a7a89d37d8c450cc1
MichaelDevries <michael.devries16095@gmail.com>
2022-12-31 12:49:33 -0600

    Added the month menu and the income section.

---
+++ main.js
@@ -0,0 +1,9 @@
+// TODO add border for the menu
+// TODO make a way to input and track expenses
+// TODO make a way to input and track incomes
+// TODO make a way to view and analyze spending
+// TODO make a way to set allotments/budget goals
+// TODO make input form 
+// TODO create categories for the types of expenses
+// TODO allow modification of the categories for future changes
+document.getElementById
